02f105e27e64f888a6b4d3e7baf3065728a3b48f
Mohamed Ashraf <mohamed.ashraf.213@gmail.com>
2015-09-27 21:22:26 +0300

    delete now works

--- calendar/routes/events.js
+++ calendar/routes/events.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-var db = require('mongoskin').db('mongodb://localhost:27017/calendar');
+var mongo = require('mongoskin');
+var ID = mongo.helper.toObjectID;
+var db = mongo.db('mongodb://localhost:27017/calendar');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -11,6 +13,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/insert/:name', function(req, res, next) {
+  console.log(req.params)
   db.collection('events').insert({name:req.params.name}, function(err, result) {
     if(result) res.send('Inserted ' + req.params.name+"\n result="+JSON.stringify(result));
   });
@@ -18,9 +21,10 @@ router.get('/insert/:name', function(req, res, next) {
 
 router.get('/:id', function(req, res, next) {
   db.collection('events').remove(
-    {_id: db.ObjectID.createFromHexString(req.params.id)},
+    {_id: ID(req.params.id)},
     function(err, result) {
       if(!err) res.send('Deleted ' + req.params.id+"\n result="+JSON.stringify(result));
+      else res.send('Error, Can\'t delete: ' + err);
     });
 });
 
