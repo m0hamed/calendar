6d3cad6b221e9078fc04ef86df1815be6f1a0ddc
Mohamed Ashraf <mohamed.ashraf.213@gmail.com>
2015-09-29 16:51:00 +0300

    Added calendar support to all event route handlers

--- calendar/routes/events.js
+++ calendar/routes/events.js
@@ -14,7 +14,6 @@ router.all('*', function(req, res, next) {
   calendar_id = ID(req.params.cal_id);
   db.collection('calendars').find({_id: calendar_id})
     .toArray(function(err, result) {
-      console.log(result);
       if(result.length==0) res.status(404).send('Calendar not found');
       else next();
     });
@@ -23,24 +22,24 @@ router.all('*', function(req, res, next) {
 router.post('/', function(req, res, next) {
   console.log(req.params);
   console.log(req.body);
-  body = req.body
-  db.collection('events').insert(_.extend(body, {"calendar_id": calendar_id}),
+  db.collection('events').insert(_.extend(req.body, {"calendar_id": calendar_id}),
                                           function(err, result) {
     if(result) res.send('Inserted ' + req.body.nickname+"\n result="+JSON.stringify(result));
   });
 });
 
 router.get('/', function(req, res, next) {
-  db.collection('events').find().toArray(function(err, result) {
-    res.send(result);
-  });
+  db.collection('events').find({calendar_id: calendar_id})
+    .toArray(function(err, result) {
+      res.send(result);
+    });
 });
 
 router.post('/:id', function(req, res, next) {
   console.log(req.params, req.body);
   db.collection('events').update(
-    {_id: ID(req.params.id)},
-    req.body,
+    {_id: ID(req.params.id), calendar_id: calendar_id},
+    _.extend(req.body, {calendar_id: calendar_id}),
     function(err, result) {
       if (!err) res.send('Updated ' + req.params.id + "\n result=" +
                          JSON.stringify(result));
