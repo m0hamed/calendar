ba5a4e51c5bc0ad985e8fae07cca3029c58b0989
Mohamed Ashraf <mohamed.ashraf.213@gmail.com>
2015-09-29 16:06:52 +0300

    Added pre-hook to all events that checks if calendar exists

--- calendar/routes/events.js
+++ calendar/routes/events.js
@@ -1,14 +1,31 @@
 var express = require('express');
-var router = express.Router();
+var router = express.Router({mergeParams:true});
 
 var mongo = require('mongoskin');
 var ID = mongo.helper.toObjectID;
 var db = mongo.db('mongodb://localhost:27017/calendar');
 
+var _ = require('lodash');
+
+var calendar_id;
+
+router.all('*', function(req, res, next) {
+  console.log("In the pre action filter");
+  calendar_id = ID(req.params.cal_id);
+  db.collection('calendars').find({_id: calendar_id})
+    .toArray(function(err, result) {
+      console.log(result);
+      if(result.length==0) res.status(404).send('Calendar not found');
+      else next();
+    });
+});
+
 router.post('/', function(req, res, next) {
   console.log(req.params);
   console.log(req.body);
-  db.collection('events').insert(req.body, function(err, result) {
+  body = req.body
+  db.collection('events').insert(_.extend(body, {"calendar_id": calendar_id}),
+                                          function(err, result) {
     if(result) res.send('Inserted ' + req.body.nickname+"\n result="+JSON.stringify(result));
   });
 });
@@ -22,7 +39,7 @@ router.get('/', function(req, res, next) {
 router.post('/:id', function(req, res, next) {
   console.log(req.params, req.body);
   db.collection('events').update(
-    {_id: ID(req.params.id)}, 
+    {_id: ID(req.params.id)},
     req.body,
     function(err, result) {
       if (!err) res.send('Updated ' + req.params.id + "\n result=" +
