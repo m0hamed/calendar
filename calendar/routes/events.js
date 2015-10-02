0b06359b5f74fa6e09dff0f3224c23347487ad33
sshihata <samy.s.shihata@gmail.com>
2015-10-02 08:58:51 +0300

    Search endpoint implemneted

--- calendar/routes/events.js
+++ calendar/routes/events.js
@@ -1,3 +1,5 @@
+"use strict"
+
 var express = require('express');
 var router = express.Router();
 
@@ -6,16 +8,22 @@ var ID = mongo.helper.toObjectID;
 var db = mongo.db('mongodb://localhost:27017/calendar');
 
 router.post('/', function(req, res, next) {
-  console.log(req.params);
-  console.log(req.body);
   db.collection('events').insert(req.body, function(err, result) {
-    if(result) res.send('Inserted ' + req.body.nickname+"\n result="+JSON.stringify(result));
+    if(result) res.send('Inserted ' + req.body.name+"\n result="+JSON.stringify(result));
   });
 });
 
 router.get('/', function(req, res, next) {
   db.collection('events').find().toArray(function(err, result) {
     res.send(result);
+  });
+});
+
+router.post('/search', function(req, res, next) {
+  var query = parse(req.body);
+  db.collection('events').find(query).toArray(function(err, result) {
+    if (!err) res.send(result);
+    else res.send("search failed: " + err);
   });
 });
 
@@ -39,6 +47,17 @@ router.delete('/:id', function(req, res, next) {
       else res.send('Error, Can\'t delete: ' + err);
     });
 });
+
+
+// should be moved to helper functions component
+// should be extended with other field. ex: time, place
+function parse(params) {
+  var query = {};
+  if (params.name) query.name = {$regex: new RegExp(".*" + params.name + ".*")};
+  if (params.place) query.place = {$regex: new RegExp(".*" + params.place + ".*")};
+  if (params.time) query.time = { $gte: params.time.after, $lte: params.time.before};
+  return query
+};
 
 
 module.exports = router;
