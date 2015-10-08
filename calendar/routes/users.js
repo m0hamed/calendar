5b00af120df9560caf4840e8702f7677eb446574
Mohamed Ashraf <mohamed.ashraf.213@gmail.com>
2015-10-08 15:50:06 +0300

    Promisifies the mongoskin module

--- calendar/routes/users.js
+++ calendar/routes/users.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-var mongo = require('mongoskin');
+var Promise = require("bluebird");
+
+var mongo = Promise.promisifyAll(require('mongoskin'));
 var ID = mongo.helper.toObjectID;
 var db = mongo.db('mongodb://127.0.0.1:27017/calendar');
 var users = db.collection('users');
@@ -14,49 +16,30 @@ var _ = require('lodash');
 var USER_PARAMS = ['username', 'password']
 
 router.post('/', function(req, res, next) {
-  users.insert(_.pick(req.body, USER_PARAMS), function(err, result) {
-    if(!err) res.send('Inserted ' + req.body.name+"\n result="
-                        +JSON.stringify(result));
-    else res.send('Failed to insert ' + req.body.name+
-                  '\n result='+JSON.stringify(err)+"\n");
-  });
+  users.insertAsync(_.pick(req.body, USER_PARAMS)).then(function(result) {
+      res.send('Inserted ' + req.body.username+"\n result="
+                        +JSON.stringify(result));}
+    ).catch(function(err){
+      res.send('Failed to insert with err' + err + "\n");
+    });
 });
 
 router.post('/login', function(req, res, next) {
-  users.find(_.pick(req.body, USER_PARAMS)).toArray(function(err, result) {
-    console.log("result=", result);
-    if(result.length==0)
-      res.status(403).send("Username or password is wrong")
-    else {
-      get_token(result[0], function(token) {
-        console.log("token", token);
-        res.send(token);
-      });
-    }
+  users.findOneAsync(_.pick(req.body, USER_PARAMS)).then(function(result) {
+    if (!result) throw "Username or password is wrong"
+    get_token(result).then((token) => res.send(token));
+  }).catch(function (err) {
+    res.status(403).send(err)
   });
 });
 
-function get_token(params, callback) {
-  sessions.findOne({user_id: params._id}, function(err, doc) {
-    console.log("doc", doc)
-    if(doc) {
-      callback(doc.token);
-    } else {
-      create_token(params, function(token){
-        callback(token);
-      });
-    }
-  });
+function get_token(params) {
+  var token = uuid.v4();
+  return sessions.insertAsync({user_id: params._id,
+                       token: token, created_at: Date.now()}).then(() => token);
 }
 
-function create_token(params, callback) {
-  var token = uuid.v4();
-  sessions.insert({user_id: params._id, token: token, created_at: Date.now},
-    function(err, result) {
-      console.log(result);
-    }
-  );
-  callback(token);
+function create_token(params) {
 }
 
 
