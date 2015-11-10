4ca3d6fab02ab46923cd18b58dc7cff8d25ab4f4
sshihata <samy.s.shihata@gmail.com>
2015-11-10 12:18:01 +0200

    changed nginx conf

--- calendar-backend/routes/api/oauth.js
+++ calendar-backend/routes/api/oauth.js
@@ -12,7 +12,7 @@ router.get('/', function(req, res, next) {
   var state = JSON.parse(req.query.state);
   var user = state.user;
   google.updateToken(user, req.query.code);
-  res.redirect('http://instacalendar.tz' + state.url + '&redir=true');
+  res.redirect('/' + state.url + '&redir=true');
 });
 
 module.exports = router;
